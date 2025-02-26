8bdeaf9e48eb6408ae5b7273156169bf41c7e934
Andre <10810328+myx0m0p@users.noreply.github.com>
2025-02-26 13:50:39 +0700

    Update PDA derivation for Metadata and Associated Token Account

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
 import {
+  address,
   getAddressEncoder,
   getProgramDerivedAddress,
   getUtf8Encoder,
@@ -14,12 +15,14 @@ export async function findMetadataPda(
   seeds: MetadataSeeds,
   config: { programAddress?: Address | undefined } = {},
 ): Promise<ProgramDerivedAddress> {
-  const {
-    programAddress = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s' as Address<'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'>,
-  } = config;
+  const { programAddress = address('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s') } = config;
   return await getProgramDerivedAddress({
     programAddress,
-    seeds: [getUtf8Encoder().encode('metadata'), getAddressEncoder().encode(seeds.mint)],
+    seeds: [
+      getUtf8Encoder().encode('metadata'),
+      getAddressEncoder().encode(programAddress),
+      getAddressEncoder().encode(seeds.mint),
+    ],
   });
 }
 
@@ -30,13 +33,18 @@ export type AssociatedTokenAccountSeeds = {
 
 export async function findAssociatedTokenPda(
   seeds: AssociatedTokenAccountSeeds,
-  config: { programAddress?: Address | undefined } = {},
+  config: { tokenProgram?: Address | undefined; programAddress?: Address | undefined } = {},
 ): Promise<ProgramDerivedAddress> {
   const {
-    programAddress = 'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL' as Address<'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'>,
+    tokenProgram = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'),
+    programAddress = address('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'),
   } = config;
   return await getProgramDerivedAddress({
     programAddress,
-    seeds: [getAddressEncoder().encode(seeds.mint), getAddressEncoder().encode(seeds.owner)],
+    seeds: [
+      getAddressEncoder().encode(seeds.owner),
+      getAddressEncoder().encode(tokenProgram),
+      getAddressEncoder().encode(seeds.mint),
+    ],
   });
 }
